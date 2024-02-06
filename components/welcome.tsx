e6a662b9d57bb1836e83eac85fa284643950c66b
rafiq <rafiq.danova@gmail.com>
2024-02-06 14:18:12 +0800

    update layout section

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -1,8 +1,7 @@
 export default function WelcomeSection() {
   return (
-    <div className="mb-10">
-      <p className="text-4xl">Hi, Saya Rafiq</p>
-      <p className="text-slate-600 mb-5"><i>Web Developer</i></p>
+    <div className="mb-10 text-right">
+      <h2 className="text-3xl text-slate-700 mb-5">Tentang Saya</h2>
       <p>Saya biasanya membuat website dengan menggunakan bahasa <i>Python</i> sebagai <i>Fullstack Developer</i>.</p>
       <p>Jika tidak sebagai <i>Fullstack Developer</i> dalam suatu project saya biasanya menangani bagian Backend.</p>
       <p>Namun saya juga tertarik dibagian <i>Frontend</i> menggunakan <i>Next JS</i></p>
