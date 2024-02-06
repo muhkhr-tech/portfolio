d408524df7590c5adea1ef2a6d6cab50e8b8ffc6
rafiq <rafiq.danova@gmail.com>
2024-02-06 14:24:28 +0800

    update layout image profile dan text profile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 export default function NavbarSection() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 mb-5 sm:mb-20">
-      <div className="flex flex-wrap items-center justify-between">
+      <div className="flex items-center justify-between">
         <Image
           src="/profile.jpeg"
           width={50}
           height={50}
           alt="Muh. Khaerur Rafiq"
-          className="rounded-full mx-auto sm:ms-0"/>
+          className="rounded-full"/>
         
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div className="" id="navbar-default">
+          <ul className="font-medium flex flex-col md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               {/* <a
                 href="#"
