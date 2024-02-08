c98cafb54e7f995918c4f605dbd2a6c815d2b258
rafiq <rafiq.danova@gmail.com>
2024-02-09 06:48:17 +0800

    migrasi ke rafcode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import WhatsappButton from "./components/button/whatsapp";
+import NavbarSection from "./components/navbar";
+import ProjectsSection from "./components/projects";
+import SkillsSection from "./components/skills";
+import WelcomeSection from "./components/welcome";
+
 export default function Home() {
   return (
-    <div>
-      DigiDev
+    <div className="max-w-screen-xl mx-auto p-4">
+      <NavbarSection />
+      <WelcomeSection />
+      <ProjectsSection />
+      <SkillsSection />
+      <WhatsappButton />
     </div>
   );
 }
