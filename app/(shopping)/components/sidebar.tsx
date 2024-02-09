7430b57676c9b7cd85a6b284db54e49f8ef966c6
rafiq <rafiq.danova@gmail.com>
2024-02-10 06:04:44 +0800

    hapus duplicate api/item

--- app/(shopping)/components/sidebar.tsx
+++ app/(shopping)/components/sidebar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function SidebarSection() {
   return (
     <ul className="menu w-56 mt-5">
-      <li className="mb-1"><Link href={"/"}>Dasbor</Link></li>
+      <li className="mb-1"><Link href={"/dashboard"}>Dasbor</Link></li>
       <li className="mb-1"><Link href={"/item"}>Barang</Link></li>
       <li className="mb-1"><Link href={"/item-type"}>Jenis Barang</Link></li>
       <li className="mb-1"><Link href={"/shop"}>Belanja</Link></li>
--- app/api/item/route.ts
+++ app/api/item/route.ts
@@ -1,9 +1,17 @@
 import { db } from "@/lib/db"
 import { Item } from "@/lib/schema"
+import { ilike } from "drizzle-orm"
 
 export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const name = searchParams.get('name')
+  let resp = null
+  
+  if (name) {
+    resp = await db.select().from(Item).where(ilike(Item.name, "%" + name + "%"))
+  } else {
+    resp = await db.select().from(Item)
+  }
 
-    const resp = await db.select().from(Item)
-    
-    return new Response(JSON.stringify(resp))
+  return new Response(JSON.stringify(resp))
 }
