3c3d2dea83b5684612a8101edff776ddb3222cd8
rafiq <rafiq.danova@gmail.com>
2024-02-09 10:32:13 +0800

    tambah navbar & layout

---
+++ app/(shopping)/components/sidebar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function SidebarSection() {
+  return (
+    <ul className="menu w-56 mt-5">
+      <li className="mb-1"><Link href={"/"}>Dasbor</Link></li>
+      <li className="mb-1"><Link href={"/item"}>Barang</Link></li>
+      <li className="mb-1"><Link href={"/item-type"}>Jenis Barang</Link></li>
+      <li className="mb-1"><Link href={"/shop"}>Belanja</Link></li>
+    </ul>
+  )
+}
